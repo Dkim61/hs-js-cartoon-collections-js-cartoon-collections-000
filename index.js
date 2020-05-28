67fc94996ab65a99a9e41bc837cc8030037a1300
david <dkim187@binghamton.edu>
2020-05-28 22:12:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function dwarfRollCall(dwarves) {
+  var list = '';
+  for(let i = 0; i < dwarves.length; i++) {
+    list += (`${i+1}. ${dwarves[i]} `)
+  }
+  return list
 }
 
+
+
 function summonCaptainPlanet(planeteerCalls){
+  var newPlanteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++) {
+    newPlanteerCalls.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return newPlanteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true
+    }
+  }
+  return false
 }
-
-function findTheCheese (foods) {
+  
+function findTheCheese(foods) {
+  for(let i = 0; i<foods.length; i++) {
+    if (foods[i] === "cheddar") {
+      return "cheddar"
+    }
+    else if (foods[i] === "gouda") {
+      return "gouda"
+    }
+    else if (foods[i] === "camembert") {
+      return "camembert"
+    }
+  }
+  return "no cheese!"
 }
