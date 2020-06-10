d605f679cb134eb74100b20be625ef9eb6fcfa36
david <dkim187@binghamton.edu>
2020-06-10 04:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
 function dwarfRollCall(dwarves) {
-  var list = '';
-  for(let i = 0; i < dwarves.length; i++) {
-    list += (`${i+1}. ${dwarves[i]} `)
-  }
-  return list
+  var dwarfList = ''
+    for (let i = (dwarves.length / 2); i < dwarves.length; i++) {
+      dwarfList += (`${i + 1}. ${dwarves[i]} `)
+    }
+    return dwarfList
 }
 
-
-
 function summonCaptainPlanet(planeteerCalls){
-  var newPlanteerCalls = [];
-  for(let i = 0; i < planeteerCalls.length; i++) {
-    newPlanteerCalls.push(`${planeteerCalls[i].toUpperCase()}!`)
-  }
-  return newPlanteerCalls
+  var newPlanetList = []
+   for (let i = 0; i < planeteerCalls.length; i++) {
+     newPlanetList.push(`${planeteerCalls[i].toUpperCase()}!`)
+   }
+   return newPlanetList
 }
 
 function longPlaneteerCalls(words) {
-  for(let i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++){
     if (words[i].length > 4) {
       return true
     }
   }
   return false
 }
-  
+//// why does the order of true/ false matter?
+//// when the question asks for "any", you should avoid using false within the For Loop
 function findTheCheese(foods) {
-  for(let i = 0; i<foods.length; i++) {
-    if (foods[i] === "cheddar") {
+  for (let i = 0; i < foods.length; i++) {
+    if(foods[i] === "cheddar") {
       return "cheddar"
-    }
-    else if (foods[i] === "gouda") {
+    } else if (foods[i] === 'gouda') {
       return "gouda"
-    }
-    else if (foods[i] === "camembert") {
+    } else if (foods[i] === 'camembert') {
       return "camembert"
+    } else if (foods[i] === 'swiss') {
+      return "swiss"
     }
   }
   return "no cheese!"
 }
+
+
+
+/// startsWithB(words)
+///
+var words = ["butter", "apple", "bread", "cheese"]
+
+function startsWithB(words){
+  var wordsWithB = []
+    for (let i = 0; i < words.length; i++){
+      if (words[i][0] === 'b') {
+        wordsWithB.push(words[i])
+      }
+    }
+  return wordsWithB
+}
+
+
+
